30cec297285700defe334a6813bba7932db76abc
xzj <329530588@qq.com>
2017-06-22 11:12:19 +0800

    Passing the Store Down with <Provider> from React Redux

--- src/index.js
+++ src/index.js
@@ -1,26 +1,9 @@
 import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import TodoApp from './App';
 import './index.css';
 import store from './stores/TodoStore.js';
-
-class Provider extends Component {
-
-	getChildContext() {
-		console.log('Provider store', this.props.store);
-		return {
-			store: this.props.store
-		}
-	}
-
-	render() {
-		return this.props.children;
-	}
-}
-
-Provider.childContextTypes = {
-	store: PropTypes.object
-}
 
 ReactDOM.render(
 	<Provider store={store}>
